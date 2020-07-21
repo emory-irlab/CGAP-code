eb9b1d22635e10252ba4c0ba0ad45a737943075a
elviskahoro <ekk0809@gmail.com>
2020-07-21 17:11:27 -0400

    c

--- stats.py
+++ stats.py
@@ -393,8 +393,9 @@ def main(
 			write_errors_to_disk(overwrite=False)
 
 	if upload_aggregate_from_folder:
+		split_folder_name: List[str] = upload_aggregate_from_folder.split(UNDERSCORE)
 		upload_aggregate_from_folder_helper(
-			filename_label=upload_aggregate_from_folder.replace("folder_stats_", ""),
+			filename_label=UNDERSCORE.join(split_folder_name[2:]),
 			folder=universal_parameters[upload_aggregate_from_folder],
 		)
 
